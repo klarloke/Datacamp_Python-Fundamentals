d25cce489b17f0e3c1684a7b1acc97f0e1fe3202
syiloke <61178915+syiloke@users.noreply.github.com>
2020-11-14 17:46:06 +0800

    Create 1.1. Python Basics.py

---
+++ 1. Introduction to Python/1.1. Python Basics.py
@@ -0,0 +1,14 @@
+## PYTHON AS A CALCULATOR
+
+# Addition, subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication, division, modulo, and exponentiation
+print(3 * 5)
+print(10 / 2)
+print(18 % 7)
+print(4 ** 2)
+
+# How much is your $100 worth after 7 years (if you can earn 10% annual interest)?
+print(100 * (1.1 ** 7))
