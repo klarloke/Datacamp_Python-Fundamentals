7af236942b0a6a0afcf10e9f4f568c6029ccff3e
syiloke <61178915+syiloke@users.noreply.github.com>
2020-11-17 03:47:34 +0800

    Update 2.1. Matplotlib.py

--- 2. Intermediate Python/2.1. Matplotlib.py
+++ 2. Intermediate Python/2.1. Matplotlib.py
@@ -1,1 +1,30 @@
+### 2.1.1. BASIC PLOTS WITH MATPLOTLIB
 
+## LINE PLOT
+
+  # Print the last item from year and pop
+  print(year[-1])
+  print(pop[-1])
+
+  # Import matplotlib.pyplot as plt
+  import matplotlib.pyplot as plt
+
+  # Make a line plot: year on the x-axis, pop on the y-axis
+  plt.plot(year, pop)
+
+  # Display the plot with plt.show()
+  plt.show()
+
+## SCATTER PLOT
+
+  # Change the line plot below to a scatter plot
+  plt.scatter(gdp_cap, life_exp)
+
+  # Put the x-axis on a logarithmic scale
+  plt.xscale('log')
+
+  # Show plot
+  plt.show()
+#-------------------------------------------------------------------------------------------------#
+
+### 2.1.2. HISTOGRAM
