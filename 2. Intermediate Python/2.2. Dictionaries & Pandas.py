ce74008145eba110387ab48fc9b8cea8da89c440
syiloke <61178915+syiloke@users.noreply.github.com>
2020-11-19 00:55:47 +0800

    Update 2.2. Dictionaries & Pandas.py

--- 2. Intermediate Python/2.2. Dictionaries & Pandas.py
+++ 2. Intermediate Python/2.2. Dictionaries & Pandas.py
@@ -13,3 +13,24 @@
 
 ### 2.2.2. CREATE DICTIONARY
 
+  #Create dictionary
+  # Definition of countries and capital
+  countries = ['spain', 'france', 'germany', 'norway']
+  capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+  # From string in countries and capitals, create dictionary europe
+  europe = { "spain": "madrid","france":"paris","germany":"berlin","norway":"oslo"}
+
+  # Print europe
+  print(europe)
+  
+### ACCESS DICTIONARY
+  
+  # Definition of dictionary
+  europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+  # Print out the keys in europe
+  print(europe.keys())  # ----> dict_keys(['spain', 'germany', 'norway', 'france'])
+
+  # Print out value that belongs to key 'norway'
+  print(europe['norway'])
