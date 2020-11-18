0d256efa5828eac27544821d17cd49b94a0a0e58
syiloke <61178915+syiloke@users.noreply.github.com>
2020-11-18 20:08:00 +0800

    Update 2.2. Dictionaries & Pandas.py

--- 2. Intermediate Python/2.2. Dictionaries & Pandas.py
+++ 2. Intermediate Python/2.2. Dictionaries & Pandas.py
@@ -1,1 +1,15 @@
+### 2.2.1 MOTIVATION FOR DICTIONARIES
+
+  # Definition of countries and capital
+  countries = ['spain', 'france', 'germany', 'norway']
+  capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+  # Get index of 'germany': ind_ger
+  ind_ger = countries.index('germany')
+
+  # Use ind_ger to print out capital of Germany
+  print(capitals[ind_ger])
+#----------------------------------------------------------------#
+
+### 2.2.2. CREATE DICTIONARY
 
